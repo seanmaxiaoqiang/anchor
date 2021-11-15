9ed87553edf45ad5cd8f09bc8093c166268eae55
seanma <seanmaxiaoqiang@icloud.com>
2021-11-15 10:17:40 -0500

    gbk file read

--- tdx2mysql/src/main.rs
+++ tdx2mysql/src/main.rs
@@ -1,3 +1,20 @@
+extern crate encoding;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use encoding::all::GBK;
+use encoding::{Encoding, EncoderTrap, DecoderTrap};
+
 fn main() {
-    println!("Hello, world!");
+     let mut file = File::open("./tdx/SH#999999.txt").expect("file not found");
+    let reader = BufReader::new(&file);
+
+    let mut lines = reader.split(b'\n').map(|l| l.unwrap());
+
+    for line in lines {
+        let decoded_string = GBK.decode(&line, DecoderTrap::Strict).unwrap();
+        println!("{}", decoded_string);
+    }
 }
