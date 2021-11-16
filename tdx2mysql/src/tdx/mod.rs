259850fce6aa5828c1f64dc184848f4406e97790
xiaoqiang.ma <xiaoqiang.ma@keystore.com>
2021-11-16 16:49:22 +0800

    data_20211116_01

--- tdx2mysql/src/tdx/mod.rs
+++ tdx2mysql/src/tdx/mod.rs
@@ -13,8 +13,44 @@ pub fn open_export_file(pathname: &str) {
 
     let lines = reader.split(b'\n').map(|l| l.unwrap());
 
+    let mut line_num = 1;
+
     for line in lines {
         let decoded_string = GBK.decode(&line, DecoderTrap::Strict).unwrap();
-        println!("{}", decoded_string);
+        line_parser(line_num, &decoded_string);
+        line_num = line_num + 1;
     }
+}
+
+fn line_parser(line_num:u32, line:&str){
+    match line_num {
+        1 => data_header(line),
+        2 => data_title(line),
+        _ => one_data(line_num, line.trim_matches('\r')),
+    }
+}
+
+fn data_header(line:&str) {
+    let ts =line.split_whitespace();
+    for s in ts {
+        println!("{}", s)
+    }
+    println!()
+}
+
+fn data_title(line:&str) {
+    let ts =line.split_whitespace();
+    for s in ts {
+        println!("{}", s)
+    }
+    println!()
+}
+
+fn one_data(line_num:u32, line:&str) {
+    let ts = line.split(",");
+    print!("{}::\"{}\"::[{}:",line_num-2, line, ts.clone().count());
+    for s in ts {
+        print!("{}:", s)
+    }
+    println!("]")
 }
